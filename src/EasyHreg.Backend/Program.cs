50eda99addf5a49e80dabbaab4cf8d409767679d
PascalKnecht <pascalknechtdev@gmail.com>
2024-08-24 12:44:31 +0200

    refactored Program.cs

--- src/EasyHreg.Backend/Program.cs
+++ src/EasyHreg.Backend/Program.cs
@@ -5,11 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<PdfService>();
 builder.Services.AddTransient<IDocumentService, FileService>();
+builder.Services.AddTransient<IDatabaseService, ExcelService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-builder.Services.AddTransient<IDatabaseService, ExcelService>();
 
 var app = builder.Build();
 
