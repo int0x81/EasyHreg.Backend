f71f971d797a2379f3dcbb23beb54dfe6ab71cae
PascalKnecht <pascalknechtdev@gmail.com>
2024-08-24 10:40:03 +0200

    adjusted PersonDto for RestApi.

--- src/EasyHreg.Backend/Models/Person.cs
+++ src/EasyHreg.Backend/Models/Person.cs
@@ -3,6 +3,13 @@ namespace EasyHreg.Backend.Models;
 public record Person
 {
     public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public Gender Gender { get; set; } = Gender.Other;
+    public string Nationality { get; set; } = string.Empty;
+    public DateTime Birthdate { get; set; }
+    public string Residence { get; set; } = string.Empty;
+    public string OfficialId { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
 }
